e756e1397b65cfdca25f33e19f8dbebe9f98c07c
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-10-31 17:14:46 -0700

    Made a windows alert show up if the price was not a number.

--- sales.js
+++ sales.js
@@ -4,6 +4,9 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
+  if (isNaN(newItem)){
+    window.alert ("Enter price as a number");
+  }
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
   //OTHERWISE,
